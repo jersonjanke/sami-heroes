5e6b49923f44cf35b20f6654f3eea1e54514dbdc
jerson janke <jersonjanke@gmail.com>
2021-04-20 22:14:52 -0300

    create basic modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../assets/styles/card.scss'
+import Modal from './Modal'
 
 const Card = ({ hero }) => {
+  const modal = useRef(null)
+
   return (
-    <div className="card">
+    <div className="card" onClick={() => modal.current.open()}>
       <div>{hero.name}</div>
       <img alt={hero.name} src={hero.image.url} />
+      <Modal ref={modal}>
+        <div>{hero.name}</div>
+      </Modal>
     </div>
   )
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,71 @@
+import React, {
+  useState,
+  useEffect,
+  forwardRef,
+  useCallback,
+  useImperativeHandle,
+} from 'react'
+import { createPortal } from 'react-dom'
+import '../assets/styles/modal.scss'
+
+const modalElement = document.getElementById('modal-root')
+
+const Modal = ({ children }, ref) => {
+  const [show, setShow] = useState(false)
+
+  const close = useCallback(() => setShow(false), [])
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      open: () => setShow(true),
+      close,
+    }),
+    [close]
+  )
+
+  const handleEscClose = (e) => {
+    if (e.key === 'Escape') {
+      setShow(false)
+    }
+  }
+
+  const handleClose = () => {
+    setShow(false)
+  }
+
+  useEffect(() => {
+    if (show) {
+      document
+        .getElementById('modal-close')
+        .addEventListener('click', handleClose, false)
+      document.addEventListener('keydown', handleEscClose, false)
+    }
+    return () => {
+      document.removeEventListener('click', handleClose, false)
+      document.removeEventListener('keydown', handleEscClose, false)
+    }
+  }, [show])
+
+  return createPortal(
+    show ? (
+      <div className="modal">
+        <div className="modal-content">
+          <span
+            role="button"
+            id="modal-close"
+            className="modal-close"
+            aria-label="close"
+            onClick={() => handleClose}
+          >
+            x
+          </span>
+          {children}
+        </div>
+      </div>
+    ) : null,
+    modalElement
+  )
+}
+
+export default forwardRef(Modal)
